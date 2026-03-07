48af542e5c7ccdcfe5a02c734dca9a5b9d093512
Egoorbis <33342912+Egoorbis@users.noreply.github.com>
2026-03-07 21:40:11 +0100

    Update outputs.tf

--- projects/outputs.tf
+++ projects/outputs.tf
@@ -1,7 +1,8 @@
 output "vending_machine_summary" {
   description = "Summary of the newly provisioned project infrastructure"
   value = {
-    project_name    = module.test_repo.repository_url
+    project_name    = module.test_repo.repo_html_url
     azure_identity  = module.test_spn.azure_client_id
  }
 }
+

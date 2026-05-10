27b848d26776d5d72181af516576f445392d3516
Egoorbis <egoorbis@users.noreply.github.com>
2026-05-10 11:12:06 +0200

    feat: add OIDC trust for GitHub Actions on pull requests

--- modules/entra-spn/main.tf
+++ modules/entra-spn/main.tf
@@ -15,6 +15,15 @@ resource "azuread_application_federated_identity_credential" "this" {
   subject        = "repo:${var.github_owner}/${var.repo_name}:ref:refs/heads/main"
 }
 
+resource "azuread_application_federated_identity_credential" "pull_request" {
+  application_id = azuread_application.this.id
+  display_name   = "github-actions-oidc-pr"
+  description    = "OIDC trust for GitHub Actions on pull requests"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://token.actions.githubusercontent.com"
+  subject        = "repo:${var.github_owner}/${var.repo_name}:pull_request"
+}
+
 resource "azurerm_role_assignment" "this" {
   scope                = "/subscriptions/${var.azure_subscription_id}"
   role_definition_name = "Contributor"
